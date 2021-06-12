b5af606c4edb33de697b49e48a6d14d912020a07
Gawsan <67488752+Gawsan@users.noreply.github.com>
2021-06-12 08:15:45 +0000

    change js submit button

--- script.js
+++ script.js
@@ -7,15 +7,13 @@ function myFunction(){
  var z=0,g=0 ,k=0;
  var arr1 ="";
  var arr2 = "";
- 
   for( var i=x;i<=y;i++){
+
     if(i%2==0){
       z+=+i;
       arr1+=i+" "
-    
-     
-    
     }
+
     else if(i%2==1){
       g+=+i;
      arr2+=i+" ";
@@ -23,7 +21,6 @@ function myFunction(){
     
     
   }
-  
   
   document.getElementById('odd').innerHTML="odd values from " +x  + "  to "+ y + " are "+arr2;
   document.getElementById('odd1').innerHTML="Sum values from " +x +"  to "+ y + " are " + g ;
